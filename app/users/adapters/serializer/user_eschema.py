99c9229b8ae1fc6048a1eaa95030b6cdabcf7a9e
Luis Montiel <93620709+MrMontiel@users.noreply.github.com>
2023-08-01 16:02:40 -0500

    Update user_eschema.py

--- app/users/adapters/serializer/user_eschema.py
+++ app/users/adapters/serializer/user_eschema.py
@@ -1,7 +1,6 @@
 from app.users.adapters.sqlalchemy.user import User,Role, Permission, PermissionsRoles
 
-
-def UserScheam(user: User)-> dict:
+def userSchema(user: User)-> dict:
     return{
         "id":user.id,
         "name":user.name,
@@ -11,43 +10,39 @@ def UserScheam(user: User)-> dict:
         "id_role":user.id_role
     }
     
-
-def UsersSchema(users: list[User])->list:
-    return[UsersSchema[user] for user in users]
-
+def usersSchema(users: list[User])->list:
+    return[userSchema(user) for user in users]
 
 
-def RoleSchema(role: Role)-> dict:
+
+def roleSchema(role: Role)-> dict:
     return{
         "id":role.id,
         "name":role.name,
         "status":role.status,
-        
     }
 
-def RolesSchema(roles: list[Role])->list:
-    return[RoleSchema[role] for role in roles]
+def rolesSchema(roles: list[Role])->list:
+    return[roleSchema(role) for role in roles]
 
-
-
-def PermissionSchema(permission: Permission)-> dict:
+def permissionSchema(permission: Permission)-> dict:
     return{
         "id":permission.id,
         "name":permission.name,
-   
     }
+    
 def PermossionsSchema(permissions: list[Permission])->list:
-    return[PermissionSchema[permission] for permission in permissions]
+    return[permissionSchema(permission) for permission in permissions]
 
 
 
 
-def PermissionsRolesSchema(permisisonsroles: PermissionsRoles)-> dict:
+def permissionsRolesSchema(permisisonsroles: PermissionsRoles)-> dict:
     return{
         "id_role":permisisonsroles.id_role,
         "id_permission":permisisonsroles.id_permission
    
     }
 
-def PermissionsRolesSchema(permissionsroles: list[PermissionsRoles])-> list:
-    return[PermissionsRolesSchema[permissionsroles] for permissionsroles in permissionsroles]
+def permissionsRolesSchema(permissionsroles: list[PermissionsRoles])-> list:
+    return[permissionsRolesSchema(permissionsroles) for permissionsroles in permissionsroles]
