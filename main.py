2dc216c6e46a9bd39b87341b11cf2fa690d864e1
jaider30 <jaidercg30@gmail.com>
2023-08-10 14:23:52 -0500

    supply-provider

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
 from fastapi import FastAPI
 # Routes
 from app.sales.adapters.routes.routes import sales
+from app.supplies.adapters.routers.routes import supplies
+from app.providers.adapters.routers.routes import providers
 
 app = FastAPI()
 
 # Add routes
 app.include_router(sales)
+
+app.include_router(supplies)
+
+
+app.include_router(providers)
