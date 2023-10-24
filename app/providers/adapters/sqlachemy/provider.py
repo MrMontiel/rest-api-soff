4ff7ee5d70e5b02e0f7139ce820441896ab2576f
jaider30 <jaidercg30@gmail.com>
2023-10-24 17:10:06 -0500

    exceptions providers - supply

--- app/providers/adapters/sqlachemy/provider.py
+++ app/providers/adapters/sqlachemy/provider.py
@@ -11,11 +11,12 @@ class Provider(Base):
   __tablename__ = "providers"
   
   id: Mapped[str] = mapped_column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
-  name: Mapped[str] = mapped_column(String, nullable=False) 
+  nit: Mapped[str] = mapped_column(String, nullable=False) 
+  name: Mapped[str] = mapped_column(String, nullable=False)
   company: Mapped[str] = mapped_column(String, nullable=False)
   address: Mapped[str] = mapped_column(String, nullable=False)
   date_registration: Mapped[datetime] = mapped_column(DateTime, default=datetime.utcnow) 
-  email: Mapped[str] = mapped_column(String, nullable=False)
+  # email: Mapped[str] = mapped_column(String, nullable=False)
   phone: Mapped[str] = mapped_column(String)
   city: Mapped[str] = mapped_column(String, nullable=False)
   status: Mapped[bool] = mapped_column(Boolean, nullable=False, default=True)
