e1f1fcbb35eeb4ee5cb754ac976a7ade36937b0e
jaider30 <jaidercg30@gmail.com>
2023-10-30 20:19:26 -0500

    provider-supply-backend

--- app/supplies/adapters/routes/routes.py
+++ app/supplies/adapters/routes/routes.py
@@ -5,6 +5,8 @@ from fastapi import APIRouter, HTTPException, status
 from app.supplies.adapters.services.services import GetAllSupplies, UpdateSupply, DeleteSupply, AddSupply, GetOneSupply, UpdateStatusSupply
 from app.supplies.adapters.sqlalchemy.supply import Supply
 from app.supplies.adapters.serializers.supply_schema import SupplySchema, suppliesSchema
+from app.auth.adapters.services.user import User, getCurrentActivateUser
+from fastapi import Depends
 
 from app.supplies.domain.pydantic.supply import SupplyCreate, SupplyUpdate, SupplyDelete
 
@@ -16,12 +18,12 @@ supplies = APIRouter(
 )
 
 @supplies.get('/')
-async def get_all_supplies(limit: int = 100, offset: int = 0):
+async def get_all_supplies(limit: int = 100, offset: int = 0, user: User = Depends(getCurrentActivateUser)):
     supplies = GetAllSupplies(limit, offset)
     return suppliesSchema(supplies)
 
 @supplies.get("/{id}")
-async def get_supply(id: str ):
+async def get_supply(id: str , user: User = Depends(getCurrentActivateUser)):
     supplies = GetOneSupply(id)
     return SupplySchema(supplies)
 
@@ -29,7 +31,7 @@ async def get_supply(id: str ):
 
 
 @supplies.post('/create_supply')
-async def create_supply(supply: SupplyCreate):
+async def create_supply(supply: SupplyCreate, user: User = Depends(getCurrentActivateUser)):
     new_supply = AddSupply(supply)
     return {
     "Supply Create": SupplySchema(new_supply)
@@ -37,14 +39,14 @@ async def create_supply(supply: SupplyCreate):
 
     
 @supplies.put('/update_supply/{id}')
-async def update_supply_route(id: str, supply_update: SupplyUpdate):
+async def update_supply_route(id: str, supply_update: SupplyUpdate, user: User = Depends(getCurrentActivateUser)):
     updated_supply = UpdateSupply(id, supply_update)
     return {"Supply Update": SupplySchema(updated_supply)
     }
     
     
 @supplies.delete('/delete_supply/{id}')
-async def delete_supply_route(id: str):
+async def delete_supply_route(id: str, user: User = Depends(getCurrentActivateUser)):
     delete_supply = DeleteSupply(id)
     return{
         "Supply Delete": SupplySchema(delete_supply)
@@ -52,7 +54,7 @@ async def delete_supply_route(id: str):
     }
     
 @supplies.put("/{id}/status_update_supply")
-async def updateStatusSupply(id:str):
+async def updateStatusSupply(id:str, user: User = Depends(getCurrentActivateUser)):
     update_supply_route = UpdateStatusSupply(id)
     return{
         "Supply update": SupplySchema(update_supply_route)
