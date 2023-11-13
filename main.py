e74e81392ea5f3983641abb1cffdc330a2856200
= <=>
2023-11-13 09:57:33 -0500

    wilchard corrección

--- main.py
+++ main.py
@@ -16,11 +16,9 @@ from app.auth.adapters.routes.routes import auth
 
 app = FastAPI()
 
-origins = ["http://localhost:3000"]
-
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["GET","PATCH","POST","PUT","DELETE", "OPTIONS"],
     allow_headers=["*"],
