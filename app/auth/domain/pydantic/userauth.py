4c79a250cad98a679d930d44792c0c131f3b6b23
Luis Montiel <93620709+ldmontielm@users.noreply.github.com>
2023-11-20 23:18:30 -0500

    Update userauth.py

--- app/auth/domain/pydantic/userauth.py
+++ app/auth/domain/pydantic/userauth.py
@@ -1,17 +1,21 @@
 from pydantic import BaseModel
+from typing import List, Optional
 
 class Credentials(BaseModel):
     username: str
     password: str
 
 class User(BaseModel):
-    name: str | None = None
+    # name: str | None = None
+    name: Optional[str] = None
     email: str
-    status: bool | None = None
+    # status: bool | None = None
+    status: Optional[str] = None
     permissions: list[str]
     
 class UserAuthenticated(User):
-    hashed_password: str | None = None
+     # hashed_password: str | None = None
+    hashed_password: Optional[str] = None
     
 class TokenResponse():
     access_token: str
