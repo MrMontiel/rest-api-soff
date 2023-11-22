62cdd7f4793a660960255f4cbc220009c9cffdf9
evelynmontoyav <evemontoya10@ielasnieves.edu.co>
2023-11-21 22:11:14 -0500

    changes

--- app/dashboard/adapters/routes/routes.py
+++ app/dashboard/adapters/routes/routes.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter, Depends, HTTPException, status
 from app.auth.adapters.services.user import getCurrentActivateUser
 from app.auth.domain.pydantic.userauth import User
-from app.dashboard.adapters.service.service import getTargetsDashboard, getGraficSales
+from app.dashboard.adapters.service.service import getPyment,getTargetsDashboard, getGraficSales
 
 
 dashboard = APIRouter(
@@ -18,3 +18,8 @@ async def get_targets():
 async def get_grafic():
   graficSales= getGraficSales()
   return graficSales
+
+@dashboard.get('/grafic_payment')
+async def get_grafic_payment():
+  graficpaymnet= getPyment()
+  return graficpaymnet
